57d8ddff1db211c0fad36378cecfda31761e5e3d
Paramvir Atwal <sparamvir4@gmail.com>
2019-06-16 21:46:51 -0400

    second commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/About', (req, res, next) => {
+  res.render('About', {});
+});
+
+router.get('/Skills', (req, res, next) => {
+  res.render('Skills', {});
+});
+
+router.get('/Work', (req, res, next) => {
+  res.render('Work', {});
+});
+
+router.get('/Projects', (req, res, next) => {
+  res.render('Projects', {});
+});
+
+
+
+
 module.exports = router;
