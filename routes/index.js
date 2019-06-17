5381380a8117775ddd6e4941f86a695eabe5fe35
Paramvir Atwal <sparamvir4@gmail.com>
2019-06-17 04:11:34 -0400

    last

--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,23 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+/* GET about page. */
 router.get('/About', (req, res, next) => {
   res.render('About', { age: 20});
 });
-
+/* GET skills page. */
 router.get('/Skills', (req, res, next) => {
   res.render('Skills', {});
 });
-
+/* GET work page. */
 router.get('/Work', (req, res, next) => {
   res.render('Work', {});
 });
-
+/* GET project page. */
 router.get('/Projects', (req, res, next) => {
   res.render('Projects', {});
 });
-
+/* GET contact page. */
 router.get('/Contact', (req, res, next) => {
   res.render('Contact', {});
 });
